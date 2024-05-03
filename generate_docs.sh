0ed617f4151d6065baaa034f700bd0ee77a825a1
Djane Rey Mabelin <thedjaney@gmail.com>
2024-05-04 00:26:49 +0800

    Update documentation

--- generate_docs.sh
+++ generate_docs.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
+# Generate docs
 DIR="$(pwd)/src"
 DOCS_DIR=docs/docs
 find $DOCS_DIR -name '*.md' -delete
@@ -18,4 +19,28 @@ EOL
     terraform-docs -c .terraform.docs.yml $DIR/$g/$m >> $FILENAME
     ((n++))
   done;
+done;
+
+# Examples
+DIR="$(pwd)/examples"
+DOCS_DIR=docs/examples
+find $DOCS_DIR -name '*.md' -delete
+n=1
+for g in $(ls $DIR); do
+  printf -v nn "%03d" $n
+  FILENAME=$DOCS_DIR/${nn}_${g}.md
+  CONTENT=""
+  if [[ -f $DIR/$g/readme.md ]]; then
+    CONTENT=$(cat $DIR/$g/readme.md)
+  fi
+  if [[ -f $DIR/$g/example.tf ]]; then
+    EXAMPLE=$(cat $DIR/$g/example.tf)
+  fi
+  cat > $FILENAME <<EOL
+${CONTENT}
+\`\`\`
+${EXAMPLE}
+\`\`\`
+EOL
+    ((n++))
 done;
